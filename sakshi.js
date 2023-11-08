2e93ff01c35cb3b07111d41dc5b6935504635037
Sakshi Karpe <140832611+sakshikarpe@users.noreply.github.com>
2023-11-07 20:42:03 -0800

    Add files via upload

---
+++ sakshi.js
@@ -0,0 +1,7 @@
+function validate()
+{
+    var name=document.getElementById("name").Value;
+    var email=document.getElementById("email").value;
+    var subject=document.getElementById("subject").value;
+    var message=Document.getElementById("message").value;
+}
